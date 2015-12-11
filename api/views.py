a4d2b0d2e48a73b2dafb235113dec108642866da
Charles BOCHET <charles.bochet@eleves.enpc.fr>
2015-12-11 20:54:17 +0100

    Création d'une application django 'api' Ecriture de la première route (/api) et affichage d'un message d'accueil

---
+++ api/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def home(request):
+    """Home page view"""
+    text = """<h1>The Vélib 2.0</h1>
+        <p>Bienvenue sur la page d'accueil de l'API</p>"""
+    return HttpResponse(text)
