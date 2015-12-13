f5a3c4ad03413827bc9a0bba8d57f00c1ce211e9
Charles BOCHET <charles.bochet@eleves.enpc.fr>
2015-12-13 13:39:07 +0100

    Création d'une classe modèle, création de la structure de la bdd

--- api/models.py
+++ api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    number = models.PositiveIntegerField()
+    contract_name = models.CharField(max_length=15)
+    name = models.CharField(max_length=40)
+    address = models.CharField(max_length=40)
+    lat = models.FloatField()
+    lng = models.FloatField()
+    banking = models.BooleanField()
+    bonus = models.BooleanField()
+    status = models.CharField(max_length=6)
+    bike_stands = models.PositiveSmallIntegerField()
+    available_bike_stands = models.PositiveSmallIntegerField
+    available_bikes = models.PositiveSmallIntegerField
+    last_update = models.DateTimeField
+
+    def __str__(self):
+        """Method returning a simple description of the object"""
+
+        return self.titre
