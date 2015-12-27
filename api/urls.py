7aa21c83e6c179f75f6d4b702afaa9366390f2cb
Charles BOCHET <charles.bochet@eleves.enpc.fr>
2015-12-27 19:05:04 +0100

    Correction encodage : utilisation utf8 forcee

--- api/urls.py
+++ api/urls.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.conf.urls import url
 from . import views
 
@@ -5,7 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home),
     url(r'^pulldata$', views.pull_data),
-    # On laisse de côté pour le moment, ça va venir avec l'API REST
+    # On laisse de cÃ´tÃ© pour le moment, Ã§a va venir avec l'API REST
     # url(r'^getstation/coordinates/(?P<lat>[+-]?\d?\d\.?\d*)/(?P<lng>[+-]?\d?\d?\d\.?\d*)$',
     #    views.get_station_coordinates),
     # url(r'^get_station/address/(?P<address>.*)$', views.getstation_address)
