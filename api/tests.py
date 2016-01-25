14fba8eb0b8fe44c2c34b27b80f81cac735e9389
Toussaint <toussaint.behaghel@gmail.com>
2016-01-25 14:22:07 +0100

    Premier tests

--- api/tests.py
+++ api/tests.py
@@ -1,7 +1,41 @@
 from django.test import TestCase
-from models.py
+from api.models import Station
+from api.views import refresh_stations
+from rest_framework.test import APIRequestFactory
 
 # Create your tests here.
+
+########################################################################################################################
+####################################          Base    Tests         ####################################################
+########################################################################################################################
+
 class RefreshTest(TestCase):
+    """
+    Test la fonction refresh_stations
+    """
     def test_refresh(self):
-        #Refresh stations
+        refreshed_db = refresh_stations()
+        self.assertEqual(refreshed_db.issues, 0)
+
+
+class StationTest(TestCase):
+    """
+    Test unitaire du modèle Station
+    """
+    def test_station(self):
+        station_test = Station(number=901,
+                        name="00901 - PORT SOLFERINO (STATION MOBILE)",
+                        address="QUAI ANATOLE FRANCE - PONT SOLFERINO - 75007 PARIS",
+                        lat=48.86138,
+                        lng=2.32442,
+                        banking=True,
+                        bonus=True,
+                        status="OPEN",
+                        contract_name="Paris",
+                        bike_stands=20,
+                        available_bike_stands=7,
+                        available_bikes=12,
+                        optimal_criterion=0.2,
+                        last_update="2016-01-24T19:17:50Z",
+                        modified_date="2016-01-24T19:26:28.424Z")
+        self.assertEqual(str(station_test), "00901 - PORT SOLFERINO (STATION MOBILE)")
